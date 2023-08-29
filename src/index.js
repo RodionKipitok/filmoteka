0b9c0a5c622f122e91d875a907997bf176a521f7
RodionKipitok <kucernukrabota@gmail.com>
2023-08-29 23:27:30 +0300

    project upload to GitHub

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import { spinnerPlay, spinnerStop } from './js/spinner';
+
+
+import { renderModalMovie } from './js/API/get-movie-info';
+
+
+
+import './js/switch-bg-theme';
+
+import { scrollFunction } from './js/button-up';
+import { refs } from './js/refs';
+
+
+
+import { onCliсkBtnWatchGallery } from './js/API/get-movie-trailer';
+
+///Запуск спінера при завантаженні
+spinnerPlay();
+window.addEventListener('load', function (e) {
+  spinnerStop();
+});
+///Закінчення спінера при завантаженні
+
+///Запуск кнопки вверх при скролі
+window.addEventListener('scroll', scrollFunction);
+
+refs.galleryMovies.addEventListener('click', onCliсkBtnWatchGallery);
