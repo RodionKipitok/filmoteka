c12b39ccfd3f630a03d3b5b97edc1b246d8ade6d
RodionKipitok <kucernukrabota@gmail.com>
2023-09-05 21:48:54 +0300

    v2

--- src/js/search-movie.js
+++ src/js/search-movie.js
@@ -1,49 +1,145 @@
 import axios from "axios";
 
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
 
+const optionsAxios = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZTczZGFlYzM1MWNjNjFkYzcyYWIzNzVjNWNiMDMyMSIsInN1YiI6IjY0ZWVlZDk3OTdhNGU2MDBmZWE3ZDM0MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AU7NFz_YUbaZZdc_gNFOqaB2ok_aPaGZ1qEgqHpx_A4'
+  }
+};
 
 const searchFrom = document.querySelector('.header__form');
 const searchInput = document.querySelector('.header__form-input');
 const headerSearchbtn = document.querySelector('.header__search-btn');
-const galleryList = document.querySelector('.gallery__list')
+const galleryList = document.querySelector('.gallery__list');
 
+window.addEventListener('load',getMovieTrends);
 
-window.addEventListener('load',getMoviestrending)
+async function getMovieTrends(nuberPage) {
 
-
-
-async function getMoviestrending() {
-
-	const response = await axios.get(`https://api.themoviedb.org/3/trending/movie/week?language=en-US&api_key=2e73daec351cc61dc72ab375c5cb0321`);
+	const response = await axios.get(`https://api.themoviedb.org/3/movie/popular?&page=${nuberPage}`,optionsAxios);
 
 	const moviesTrending = response.data.results;
-	
-	console.log(moviesTrending);
 
-	drawingTrendsOntheMainpage(moviesTrending)
-	
+	drawingTrendsOnTheMainPage(moviesTrending)
+
 };
 
 
 
 
-
-
-
-function drawingTrendsOntheMainpage(moviesTrending) {
+function drawingTrendsOnTheMainPage(moviesTrending) {
 
 const markup = moviesTrending.map((movie) => {
-	return	 `<li><div class="card"><a href="">
-      <img src="https://image.tmdb.org/t/p/w342${movie.backdrop_path}" alt="${movie.title}" srcset="">
+
+  if (movie.poster_path !== null) {
+    return	 `<li><div class="card"><a href="">
+      <img src="https://image.tmdb.org/t/p/w342${movie.poster_path}" alt="${movie.title}" srcset="">
     </a>
     <div class="title">
       <h3>${movie.title}</h3>
       <p>${movie.release_date}</p>
     </div>
-  </div></li>`
+  </div></li>`;
+    
+  } else {
+    return '';
+  }
+	
 	}).join('');
 
-  galleryList.insertAdjacentHTML("beforeend", markup);
+  galleryList.innerHTML = markup;
 
-	//console.log(markup);
+};
+
+
+
+
+let searchQuery = '';
+
+searchFrom.addEventListener('submit',getSearchMovie);
+
+
+async function getSearchMovie(event) {
+  
+  event.preventDefault();
+
+	searchQuery = event.currentTarget.searchQuery.value;
+  //console.log(searchQuery);
+
+  const response = await axios.get(`https://api.themoviedb.org/3/search/collection?query=${searchQuery}&include_adult=false&language=en-US&page=1`,optionsAxios)
+  //console.log(response);
+
+  const movieSearchAnswer = response.data.results;
+  console.log(movieSearchAnswer);
+  
+  drawingSearchMovieOnThePage(movieSearchAnswer);
+ 
+  
+  
+  
+};
+
+
+function drawingSearchMovieOnThePage(movieSearchAnswer) {
+
+  const markup = movieSearchAnswer.map((movie) => {
+
+    if (movie.poster_path !== null) {
+      return	 `<li><div class="card"><a href="">
+        <img src="https://image.tmdb.org/t/p/w342${movie.poster_path}" alt="${movie.name}" srcset="">
+      </a>
+      <div class="title">
+        <h3>${movie.name}</h3>
+        
+      </div>
+    </div></li>`;
+      
+    } else {
+      return '';
+    }
+    
+    }).join('');
+  
+    galleryList.innerHTML = markup;
+  
+};
+
+
+
+const optionsPagination = {
+
+  totalItems: 500,
+  itemsPerPage: 10,
+  visiblePages: 5
+
+
+}
+
+const paginationDiv  = document.getElementById('pagination1');
+
+console.log(paginationDiv);
+
+const instance = new Pagination(paginationDiv,optionsPagination);
+
+
+paginationDiv.addEventListener('click',getNumberPage);
+
+function getNumberPage(event) {
+
+  if(event.target.tagName === 'A'){
+ 
+  let numberPage = event.target.textContent;
+  
+  
+    console.log(numberPage); 
+    getMovieTrends(numberPage)   
+
+  };
+
+  event.preventDefault();
+  
 };
